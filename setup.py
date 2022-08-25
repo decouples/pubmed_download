8f0dd2f1d6f78609ea8e2055ad2e9334eab258ab
lin <lilinxr@gmail.com>
2022-08-25 20:56:19 +0800

    Add files via upload
    update requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = '0.1'
+version = '0.2.1'
 
 setuptools.setup(
     name="pubmed_download",
@@ -16,6 +16,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/decouples/pubmed_download",
     packages=setuptools.find_packages(),
+    install_requires=['setuptools',
+                      'pandas',
+                      'lxml',
+                      'requests',
+                      'PyPDF2',
+                      'bs4'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
